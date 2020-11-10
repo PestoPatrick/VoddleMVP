bfd5de1fd8c95a33def25f1208d4c6954b646d69
patrick <paddymcpherson@hotmail.com>
2020-11-10 11:13:47 +0000

    managed to get dashjs working

---
+++ src/app/video/video.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+declare const dashjs: any;
+
+@Component({
+  selector: 'app-video',
+  templateUrl: './video.component.html',
+  styleUrls: ['./video.component.scss']
+})
+export class VideoComponent implements OnInit {
+
+  constructor() { }
+
+
+
+ngOnInit(): void {
+  const url = 'https://dash.akamaized.net/envivio/EnvivioDash3/manifest.mpd';
+  const player = dashjs.MediaPlayer().create();
+  player.initialize(document.querySelector('#videoPlayer'), url, true);
+  }
+
+
+}
